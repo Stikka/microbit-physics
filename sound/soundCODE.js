a169c28ea3f741da956a26c62dc132b88f089f0b
Stikka <45919421+Stikka@users.noreply.github.com>
2018-12-17 17:48:08 +0100

    Create soundCODE.js

---
+++ sound/soundCODE.js
@@ -0,0 +1,12 @@
+input.onButtonPressed(Button.AB, function () {
+    music.playTone(1000, 500)
+    basic.showNumber(1000)
+})
+input.onButtonPressed(Button.A, function () {
+    music.playTone(100, 500)
+    basic.showNumber(100)
+})
+input.onButtonPressed(Button.B, function () {
+    music.playTone(10000, 500)
+    basic.showNumber(10000)
+})
