3a6e96e0adf16a2e9ac80c7b081a68134d13faac
Stikka <45919421+Stikka@users.noreply.github.com>
2018-12-17 17:37:41 +0100

    Create photoelectriceffect2CODE.js

---
+++ photoelectriceffect/photoelectriceffect2CODE.js
@@ -0,0 +1,19 @@
+basic.forever(function () {
+    if (input.lightLevel() > 200) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . # . .
+            . . . . .
+            . . . . .
+            `)
+    } else {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            `)
+    }
+})
